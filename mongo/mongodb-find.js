ddb2a26553a6e896758431341fb1dd0ffda8e8a8
shayanmalinda <shayanmalinda@gmail.com>
2020-04-11 11:00:07 +0530

    fetch data

--- mongo/mongodb-find.js
+++ mongo/mongodb-find.js
@@ -1,22 +1,24 @@
 const MongoClient = require("mongodb");
+
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
   if (err) {
     return console.log("Unable to connect to MongoDB Server");
   }
 
   console.log("Connected to MongoDB Server");
-  db.collection("Todos").insert(
-    {
-      test: "Something to do 2",
-      completed: false,
-    },
-    (err, result) => {
-      if (err) {
-        return console.log("Unable to insert off", err);
+
+  db.collection("Todos")
+    .find()
+    .toArray()
+    .then(
+      (docs) => {
+        console.log("Todos");
+        console.log(JSON.stringify(docs, undefined, 2));
+      },
+      (err) => {
+        console.log("Unable to fetch data", err);
       }
-      console.log(result.ops[0]._id);
-    }
-  );
+    );
 
   db.close();
 });
